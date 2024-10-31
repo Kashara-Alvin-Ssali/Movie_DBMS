220a8f7f71424c4381bb1f7ac6b18d5db7b5a2c6
unknown <alvinkashara20@gmail.com>
2024-10-31 12:25:56 +0300

    Project first commit

---
+++ website/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . models import *
+
+admin.site.register(Category)
+admin.site.register(Video)
+admin.site.register(Members)
+admin.site.register(RentedVideo)
+admin.site.register(Branch)
+admin.site.register(Staff)
+
+
+# Register your models here.
